3e7fa89e7b56df1c312d74367fb271aa5535cb63
Petar Kovacevic <petar.kovacevic0088@gmail.com>
2021-04-15 15:45:36 +0200

    email-service initialization

---
+++ Back-end/email-service/src/test/java/com/example/student2students/emailservice/EmailServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student2students.emailservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
