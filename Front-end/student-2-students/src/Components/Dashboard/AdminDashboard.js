16d9f2bc244269e2b1ebc6dd4a287a650c9c98d9
Petar Kovacevic <petar.kovacevic0088@gmail.com>
2021-04-16 01:32:52 +0200

    Charts

--- Front-end/student-2-students/src/Components/Dashboard/AdminDashboard.js
+++ Front-end/student-2-students/src/Components/Dashboard/AdminDashboard.js
@@ -3,17 +3,19 @@ import Navbar from '../Navbar/Navbar'
 import axios from 'axios'
 import { API_ENDPOINT } from '../Constants/Endpoints'
 import { useHistory } from 'react-router-dom'
+import '../../css/AdminDashboard.css'
+import LineChart from '../Charts/LineChart'
 
 function AdminDashboard() {
     const history = useHistory();
 
-    useEffect(() => {
-        axios.get(API_ENDPOINT + '/authorization/admin', { withCredentials: true })
-            .catch(err => {
-                console.log(err);
-                history.push('/');
-            })
-    }, [history])
+    // useEffect(() => {
+    //     axios.get(API_ENDPOINT + '/authorization/admin', { withCredentials: true })
+    //         .catch(err => {
+    //             console.log(err);
+    //             history.push('/');
+    //         })
+    // }, [history])
 
     const logout = () => {
         axios.get(API_ENDPOINT + '/authorization/logout', { withCredentials: true });
@@ -23,8 +25,13 @@ function AdminDashboard() {
     return (
         <div>
             <Navbar />
-            <h1> Admin Dashboard! </h1>
-            <button onClick={logout} > Logout </button>
+            <div className="content">
+                <h1 id="title"> Admin Dashboard </h1>
+                <div className="chart">
+                    <LineChart />
+                </div>
+                <button id="logout" onClick={logout} > Logout </button>
+            </div>
         </div>
     )
 }
