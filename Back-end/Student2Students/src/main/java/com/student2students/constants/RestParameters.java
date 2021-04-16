bde5455a730990d6d6aad162323d9f185fd1a503
Petar Kovacevic <petar.kovacevic0088@gmail.com>
2021-04-16 18:41:13 +0200

    Service subscription to Eureka in docker containers

--- Back-end/Student2Students/src/main/java/com/student2students/constants/RestParameters.java
+++ Back-end/Student2Students/src/main/java/com/student2students/constants/RestParameters.java
@@ -3,5 +3,5 @@ package com.student2students.constants;
 public final class RestParameters {
     public static final String PAGE = "page";
     public static final String LIMIT = "limit";
-    public static final String POSTING_SERVICE_URL = "http://localhost:8081";
+    public static final String POSTING_SERVICE_URL = "http://localhost:8082";
 }
