c0e64720335f3def768d441a05b1210f9bb74d35
Petar Kovacevic <petar.kovacevic0088@gmail.com>
2021-04-20 19:23:05 +0200

    Contact information

--- Front-end/student-2-students/src/Components/Footer/Footer.js
+++ Front-end/student-2-students/src/Components/Footer/Footer.js
@@ -1,10 +1,29 @@
 import React from 'react'
 import '../../css/Footer.css'
+import { AiFillGithub } from 'react-icons/ai';
+import { SiGmail } from 'react-icons/si';
 
 function Footer() {
     return (
         <div className="footer">
-            <h2 id='company'> <span id="rights">®</span> Student2Students </h2>
+            <h2 id='company'> Copyright <span id="rights">®</span> 2021 Student2Students. All rights reserved </h2>
+            <div id="data-section">
+                <div id="left-section">
+                    <h1 id="address-heading"> Address </h1>
+                    <p className="address"> 21000, Novi Sad</p>
+                    <p className="address"> Serbia</p>
+                </div>
+                <div id="right-section">
+                    <h1 id="contact-heading"> Contact </h1>
+                    <a className="contact" href="https://github.com/JohnnyQuidd"
+                        target="_blank">
+                        <span className="icon">
+                            <AiFillGithub style={{ height: '2.5rem', width: '2.5rem' }} />
+                            </span> <span id="data-span"> github@JohnnyQuidd</span></a>
+                    <a className="contact" href="mailto:petar.kovacevic0099@gmail.com"> <span className="icon"> <SiGmail style={{height: '1.5rem', width: '1.5rem'}} /> </span>
+                         <span id="data-span"> petar.kovacevic0099@gmail.com </span></a>
+                </div>
+            </div>
         </div>
     )
 }
