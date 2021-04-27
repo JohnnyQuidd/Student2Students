5ce57e111b833f93fdf560e46b571b9340634127
Petar Kovacevic <petar.kovacevic0088@gmail.com>
2021-04-27 02:20:52 +0200

    Large screen UI changes

--- Front-end/student-2-students/src/Components/Exchange/Exchange.js
+++ Front-end/student-2-students/src/Components/Exchange/Exchange.js
@@ -9,6 +9,7 @@ import Select from 'react-select'
 import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 import { toast } from 'react-toastify';
+import Picker from 'emoji-picker-react';
 
 
 toast.configure()
@@ -26,6 +27,10 @@ function Exchange() {
     const [exchangeStart, setExchangeStart] = useState(null);
     const [exchangeEnding, setExchangeEnding] = useState(null);
     const [price, setPrice] = useState(0);
+    const [body, setBody] = useState("");
+
+    const [emojiPicker, setEmojiPicker] = useState("emoji-picker hide");
+    const [chosenEmoji, setChosenEmoji] = useState(null);
 
     const history = useHistory();
 
@@ -74,6 +79,17 @@ function Exchange() {
             })
     }, [])
 
+    const toggleEmojiPicker = () => {
+        emojiPicker === 'emoji-picker hide' ? setEmojiPicker('emoji-picker') : setEmojiPicker('emoji-picker hide');
+    }
+
+    const onEmojiClick = (event, emojiObject) => {
+        setChosenEmoji(emojiObject);
+        let text = body;
+        text = text + emojiObject.emoji;
+        setBody(text);
+      };
+
     const selectedCountryHandler = event => {
         if(event !== null) {
             setCountry(event.value);
@@ -95,7 +111,7 @@ function Exchange() {
     }
 
     const submitExchange = () => {
-        const payload = {country, city, university, minNumberOfAttendees, maxNumberOfAttendees, exchangeStart, exchangeEnding, price};
+        const payload = {country, city, university, minNumberOfAttendees, maxNumberOfAttendees, exchangeStart, exchangeEnding, price, body};
 
         axios({
             url: API_ENDPOINT + '/posting/exchange',
@@ -112,97 +128,114 @@ function Exchange() {
     return (
         <>
           <Navbar role={role} />
-          <div className="new-exchange-wrapper">
+            <div className="new-exchange-wrapper">
               <div className="new-exchange-title">
                 <h1> Have an exchange to offer? </h1>
               </div>
-            <div className="new-exchange-form">
-                <label className="new-exchange-label">Country</label>
-                <Select
-                    closeMenuOnSelect={true}
-                    className="basic-single"
-                    classNamePrefix="select"
-                    defaultValue={country}
-                    isClearable={true}
-                    isSearchable={true}
-                    name="country"
-                    isMulti={false}
-                    options={countrySelection}
-                    onChange={selectedCountryHandler}
-                    />
-            </div>
-                <div className="new-exchange-form">
-                    <label className="new-exchange-label">City</label>
-                    <input className="exchange-input" type="text" value={city} onChange={e => setCity(e.target.value)} />
+              <div className="exchange-content">
+                <div className="exchange-left-panel">
+                    <div className="new-exchange-form">
+                        <label className="new-exchange-label">Country</label>
+                        <Select
+                            closeMenuOnSelect={true}
+                            className="basic-single"
+                            classNamePrefix="select"
+                            defaultValue={country}
+                            isClearable={true}
+                            isSearchable={true}
+                            name="country"
+                            isMulti={false}
+                            options={countrySelection}
+                            onChange={selectedCountryHandler}
+                            />
+                    </div>
+                        <div className="new-exchange-form">
+                            <label className="new-exchange-label">City</label>
+                            <input className="exchange-input" type="text" value={city} onChange={e => setCity(e.target.value)} />
+                        </div>
+                    <div className="new-exchange-form">
+                        <label className="new-exchange-label">Afilliated University</label>
+                        <Select
+                            closeMenuOnSelect={true}
+                            className="basic-single"
+                            classNamePrefix="select"
+                            defaultValue={university}
+                            isClearable={true}
+                            isSearchable={true}
+                            name="university"
+                            isMulti={false}
+                            options={universitySelection}
+                            onChange={selectedUniversityHandler}
+                            />
+                    </div>
+                    <div className="new-exchange-form">
+                        <label className="new-exchange-label">Min number of Attendees ({minNumberOfAttendees}) </label>
+                        <input 
+                            className="exchange-input"
+                            id="minNumberOfAttendees" 
+                            type="range" 
+                            min="0" max="100" 
+                            value={minNumberOfAttendees} 
+                            onChange={handleMinAttendees}
+                            step="1"/>
+                    </div>
+
+                    <div className="new-exchange-form">
+                        <label className="new-exchange-label">Max number of Attendees ({maxNumberOfAttendees})</label>
+                        <input 
+                            className="exchange-input"
+                            id="maxNumberOfAttendees" 
+                            type="range" 
+                            min="0" max="100" 
+                            value={maxNumberOfAttendees} 
+                            onChange={handleMaxAttendees}
+                            step="1"/>
+                    </div>
                 </div>
-            <div className="new-exchange-form">
-                <label className="new-exchange-label">Afilliated University</label>
-                <Select
-                    closeMenuOnSelect={true}
-                    className="basic-single"
-                    classNamePrefix="select"
-                    defaultValue={university}
-                    isClearable={true}
-                    isSearchable={true}
-                    name="university"
-                    isMulti={false}
-                    options={universitySelection}
-                    onChange={selectedUniversityHandler}
-                    />
-            </div>
-            <div className="new-exchange-form">
-                <label className="new-exchange-label">Min number of Attendees ({minNumberOfAttendees}) </label>
-                <input 
-                    className="exchange-input"
-                    id="minNumberOfAttendees" 
-                    type="range" 
-                    min="0" max="100" 
-                    value={minNumberOfAttendees} 
-                    onChange={handleMinAttendees}
-                    step="1"/>
-            </div>
 
-            <div className="new-exchange-form">
-                <label className="new-exchange-label">Max number of Attendees ({maxNumberOfAttendees})</label>
-                <input 
-                    className="exchange-input"
-                    id="maxNumberOfAttendees" 
-                    type="range" 
-                    min="0" max="100" 
-                    value={maxNumberOfAttendees} 
-                    onChange={handleMaxAttendees}
-                    step="1"/>
-            </div>
+                <div className="exchange-right-panel">
+                    <div className="new-exchange-form">
+                        <label className="new-exchange-label"> From </label>
+                        <DatePicker
+                            className="exchange-date"
+                            selected={exchangeStart}
+                            onChange={date => setExchangeStart(date)}
+                            isClearable
+                            dateFormat="dd/MM/yyyy"
+                            minDate={new Date()} />
+                    </div>
 
-            <div className="new-exchange-form">
-                <label className="new-exchange-label"> From </label>
-                <DatePicker
-                    className="exchange-date"
-                    selected={exchangeStart}
-                    onChange={date => setExchangeStart(date)}
-                    isClearable
-                    dateFormat="dd/MM/yyyy"
-                    minDate={new Date()} />
-            </div>
+                    <div className="new-exchange-form">
+                        <label className="new-exchange-label"> Untill </label>
+                        <DatePicker
+                            className="exchange-date"
+                            selected={exchangeEnding}
+                            onChange={date => setExchangeEnding(date)}
+                            isClearable
+                            dateFormat="dd/MM/yyyy"
+                            minDate={exchangeStart} />
+                    </div>
 
-            <div className="new-exchange-form">
-                <label className="new-exchange-label"> Untill </label>
-                <DatePicker
-                    className="exchange-date"
-                    selected={exchangeEnding}
-                    onChange={date => setExchangeEnding(date)}
-                    isClearable
-                    dateFormat="dd/MM/yyyy"
-                    minDate={exchangeStart} />
-            </div>
+                    <div className="new-exchange-form">
+                        <label className="new-exchange-label"> Price ($) </label>
+                        <input className="exchange-input" type="number" value={price} onChange={e => setPrice(e.target.value)} />
+                    </div>
+                    
+                    <div className="new-exchange-form">
+                        <label className="new-exchange-label"> Exchange description </label>
+                        <textarea className="exchange-text-area" value={body} onChange={e => setBody(e.target.value)} />
+                        <button onClick={toggleEmojiPicker} id="exchange-emoji-button"> Add emoji </button>
+                            <div className={emojiPicker}>
+                                <Picker onEmojiClick={onEmojiClick} />
+                            </div>
+                    </div>
 
-            <div className="new-exchange-form">
-                <label className="new-exchange-label"> Price ($) </label>
-                <input className="exchange-input" type="number" value={price} onChange={e => setPrice(e.target.value)} />
-            </div>
-            <div className="new-exchange-form">
-                <button className="offer-exchange-button"  onClick={submitExchange}> Offer an exchange </button>
-            </div>
+                    <div className="new-exchange-form">
+                        <button className="offer-exchange-button"  onClick={submitExchange}> Offer an exchange </button>
+                    </div>
+                </div>
+              </div>
+
 
             </div>
           <Footer />  
